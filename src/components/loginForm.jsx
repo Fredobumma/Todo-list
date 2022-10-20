30c21e168377d83107a5c7d4cd188038abc3dba7
MANFRED OBUMMA <FREDOBUMMA@gmail.com>
2022-10-20 01:19:59 +0100

    --feat: add todo logo

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import Joi from "joi-browser";
 import PropTypes from "prop-types";
+import AppLogo from "./common/appLogo";
+import WelcomeMessage from "./common/welcomeMessage";
 import Copyright from "./common/copyright";
 import { getHooks } from "../utilities/getHooks";
 
@@ -16,8 +18,9 @@ const LoginForm = ({ form, navigate }) => {
   return (
     <main className="flex items-center justify-center h-screen">
       <div className="w-full max-w-md m-auto">
+        <AppLogo />
+        <WelcomeMessage />
         <div className="bg-white shadow-md rounded p-8 mb-4 mx-5">
-          {/* <p>TODO</p> */}
           <form onSubmit={validate.handleSubmit}>
             {validate.renderInput(
               "username",
--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -81,7 +81,7 @@ const TodoList = () => {
   };
 
   return (
-    <section className="min-h-screen bg-blue max-w-5xl mx-auto">
+    <section className="min-h-screen bg-blue max-w-5xl mx-1 lg:mx-auto">
       <header className="bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-1 xl:py-6">
         <Heading username={data.username} tasksCount={tasksCount} />
         <form>
